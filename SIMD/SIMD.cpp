54380eb4ac9d85a7635248f770f1baf8b98f8028
Noman Shahid <noman-shahid@hotmail.com>
2019-03-11 19:53:39 -0700

    Playing with intrinsics, adding 2 sets of floats

--- SIMD/SIMD.cpp
+++ SIMD/SIMD.cpp
@@ -1,24 +1,34 @@
 #include "stdafx.h"
 
-string get_cpu_name() {
-	uint32_t data[4] = { 0 };
+void assembler() {
+	float f1[] = { 1.f,2.f,3.f,4.f };
+	float f2[] = { 5.f,4.f,3.f,2.f };
+	float result[4] = { 0.f };
 
 	_asm {
-		cpuid;
-		mov data[0], ebx;
-		mov data[4], edx;
-		mov data[8], ecx;
+		movups xmm1, f1;
+		movups xmm2, f2;
+		mulps xmm1, xmm2;
+		movups result, xmm1;
 	}
 
-	return string((const char *)data);
+	for (size_t i = 0; i < 4; i++) {
+		cout << result[i] << "\t";
+	}
+	cout << endl;
 }
 
-void assembler() {
-	cout << "CPU is " << get_cpu_name() << endl;
+void intrinsics() {
+	auto a = _mm_set_ps(1, 2, 3, 4);
+	auto b = _mm_set_ps(1, 2, 3, 4);
+	auto c = _mm_add_ps(a,b);
+
+	float f = c.m128_f32[0]; // Get first value
 }
 
 int main(int argc, char* argv[]) {
-	assembler();
+	//assembler();
+	intrinsics();
 
 	getchar();
 	return 0;
